205b72d4e36bf5fbb19090ce28617ad80c61b9a4
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2019-06-17 16:48:15 -0700

    Initial commit

---
+++ NullableConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NullableConsole
+{
+    class Program
+    {
+#nullable enable
+        static void Main(string[] args)
+        {
+            string? one = Environment.GetEnvironmentVariable("one");
+            string? two = Environment.GetEnvironmentVariable("two");
+            if (one == null && two == null)
+            {
+                Console.WriteLine("both null");
+            }
+            else if (one != null && two != null)
+            {
+                Console.WriteLine("both non-null");
+            }
+            else if (one != null)
+            {
+                Console.WriteLine("one is non-null");
+            }
+            else
+            {
+                Console.WriteLine(two.Length);
+            }
+        }
+#nullable disable
+    }
+}
